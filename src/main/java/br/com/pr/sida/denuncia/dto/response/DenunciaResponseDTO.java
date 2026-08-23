72273f8c0a5ece874e5d4c8141923ffa951048b4
= <=>
2026-08-23 18:42:21 -0300

    feature/sistema de acesso a denuncias atualizado com novas entitys

--- src/main/java/br/com/pr/sida/denuncia/dto/response/DenunciaResponseDTO.java
+++ src/main/java/br/com/pr/sida/denuncia/dto/response/DenunciaResponseDTO.java
@@ -1,10 +1,10 @@
 package br.com.pr.sida.denuncia.dto.response;
 
-import br.com.pr.sida.mensagem.denuncia.MensagemDenuncia;
 import br.com.pr.sida.mensagem.denuncia.dto.response.MensagensDenunciaResponseDTO;
-import br.com.pr.sida.util.Genero;
-import br.com.pr.sida.util.PreferenciaEnvio;
-import br.com.pr.sida.util.Status;
+import br.com.pr.sida.status.StatusDenuncia;
+import br.com.pr.sida.status.dto.response.StatusDenunciaResponseDTO;
+import br.com.pr.sida.util.OndeOcorreu;
+import br.com.pr.sida.util.RedeEnsino;
 import br.com.pr.sida.util.TipoViolencia;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,13 +17,14 @@ import java.util.List;
 public class DenunciaResponseDTO{
     private Long id;
     private LocalDate dataCriacao;
-    private String nomeMunicipio;
     private String nomeEscola;
-    private int idadeDenunciante;
-    private Genero generoDenunciante;
-    private boolean violenciaNaEscola;
+    private RedeEnsino redeEnsino;
+    private String orgaoCompetenteNome;
+    private OndeOcorreu ondeOcorreu;
     private TipoViolencia tipoViolencia;
-    private Status statusDenuncia;
-    private PreferenciaEnvio preferenciaEnvio;
+    private boolean riscoAgressao;
+    private boolean situacaoGrave;
+    private boolean violacaoDireitos;
+    private List<StatusDenunciaResponseDTO> statusDenuncias;
     private List<MensagensDenunciaResponseDTO> mensagens;
 }
