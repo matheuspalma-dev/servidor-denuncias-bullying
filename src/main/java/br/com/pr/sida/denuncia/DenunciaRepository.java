7072333ce30cf96c1db0e1f820fc75a116d9dd92
= <=>
2026-08-30 15:15:27 -0300

    corrigindo erro 403 quando denuncia é feita

--- src/main/java/br/com/pr/sida/denuncia/DenunciaRepository.java
+++ src/main/java/br/com/pr/sida/denuncia/DenunciaRepository.java
@@ -3,6 +3,10 @@ package br.com.pr.sida.denuncia;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 interface DenunciaRepository extends JpaRepository<Denuncia, Long> {
+
+    public List<Denuncia> findAllByEscolaId(Long escolaId);
 }
